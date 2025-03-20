d7124526e61f4363ad05e87fd2d0d74a4f3351e2
Law2point0 <Caidentap10@gmail.com>
2025-03-20 10:51:02 +0000

    View-Projects page

---
+++ Autodesk Asset Manager/Pages/View-Projects.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Projects</title>
+</head>
+<body>
+    <?php
+    include ("Navbar.php");
+    ?>
+    <div class="container">
+        
+</body>
+</html>
