e6211ffc2941a338aaf8b18e6990a0b4beb7e70e
Joshua Charles <jcharlesbusiness22@gmail.com.>
2025-04-05 17:44:46 +0200

    Added a page for storing code meant to kick people out if they aren't logged in

---
+++ Autodesk Asset Manager/Pages/Access.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if (!isset($_SESSION['UserID']) || $_SESSION['AccessLevel'] !== "Admin") {
+    // Redirect back to login page if no session is found or Access is wrong. Replace the "Admin" with whatever is appropriate for the page.
+    header("Location: http://localhost/Autodesk-Asset-Management/Autodesk%20Asset%20Manager/Pages/Login.php");
+    exit;
+}
+?>
